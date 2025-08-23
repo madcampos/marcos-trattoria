05a3d1417c0c4a8faa17846530346f3d0b619f67
madcampos <me@madcampos.dev>
2025-08-23 06:10:03 -0400

    feat: add missing handler for share target

--- src/sw.js
+++ src/sw.js
@@ -62,6 +62,8 @@ self.addEventListener('install', (/** @type {ExtendableEvent}*/ event) => {
 		const cache = await caches.open(CACHE_VERSION);
 
 		await cache.addAll(CACHE_MANIFEST);
+
+		await self.skipWaiting();
 	})());
 });
 
@@ -84,6 +86,18 @@ self.addEventListener('activate', (/** @type {ExtendableEvent} */ event) => {
 self.addEventListener('fetch', (/** @type {FetchEvent} */ event) => {
 	event.respondWith((async () => {
 		const { request, preloadResponse } = event;
+		const url = new URL(request.url);
+
+		if (request.method === 'POST' && url.pathname.startsWith('/contact')) {
+			const title = url.searchParams.get('title') ?? '';
+			const text = url.searchParams.get('text') ?? '';
+
+			localStorage.setItem('contact-form-title', title);
+			localStorage.setItem('contact-form-text', text);
+
+			return Response.redirect('/contact/', 303);
+		}
+
 		const cache = await caches.open(CACHE_VERSION);
 
 		let response = await caches.match(request, {
